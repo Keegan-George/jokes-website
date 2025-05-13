49e5e1d23a69686d40e307851539da1dd5c0c8f0
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-05-13 07:30:54 -0400

    Initial commit

---
+++ joke.py
@@ -0,0 +1,19 @@
+"""
+A class that contains a single function for retrieving two-part jokes from the JokeAPI.
+"""
+
+import requests
+
+JOKE_API_URL = "https://v2.jokeapi.dev/joke/Any"
+
+BLACK_LIST_FLAGS = ["nsfw", "religious", "political", "racist", "sexist", "explicit"]
+
+PARAMS = {"type": "twopart", "blacklistFlags": ",".join(BLACK_LIST_FLAGS)}
+
+
+def get_joke() -> tuple[str, str]:
+    """retrieves a two-part joke"""
+
+    response = requests.get(url=JOKE_API_URL, params=PARAMS)
+    joke = response.json()
+    return (joke["setup"], joke["delivery"])
---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+A website that queries a new joke, from the Joke API, each time the clown image is clicked. 
+"""
+
+from joke import get_joke
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        joke = get_joke()
+        return render_template("index.html", joke=joke)
+
+    return render_template("index.html", joke=None)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
